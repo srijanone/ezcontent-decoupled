7a9a413f9ff7555b6d8e4c236a144b3a78be2039
Abhinesh Dhiman <abhinesh.dhiman@srijan.net>
2023-06-15 15:41:27 +0530

    ChangesForTheTgr

--- components/Header/index.js
+++ components/Header/index.js
@@ -4,10 +4,9 @@ import get from "lodash/get";
 export default props => {
   let headerData = get(props, "data.main");
   let subMenu;
-  
 
-  if(headerData) {
-    subMenu =  headerData.filter(function(item) {
+  if (headerData) {
+    subMenu = headerData.filter(function (item) {
       return item.parent != null;
     });
   }
@@ -20,62 +19,63 @@ export default props => {
 
   return (
     <>
-    <nav className="navbar fixed-top navbar-expand-lg ezcontent-header">
-      <div className="container">
-        <a className="navbar-brand logo" href="/">
-          <img src="/logo.svg" />
-        </a>
-        <button
-          className="navbar-toggler navbar-toggler-right navbar-light"
-          type="button"
-          data-toggle="collapse"
-          data-target="#CollapsingNavbar"
-          aria-controls="navbar-collapse"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div id="CollapsingNavbar" className="navbar-collapse collapse">
-          {headerData &&
-            <nav className="form-inline navbar-form">
-              <ul className="navbar-nav nav">
-                {headerData.map(menuItem => {
-                  return (
-                    !menuItem.parent && <li className="nav-item" key={menuItem.drupal_internal__id}>
-                      <a className="nav-link px-md-3" href={menuItem.link.url}>
-                        {menuItem.title}
-                      </a>
-                      {subMenu && <ul className="submenu">
-                      {subMenu.map(submenuItem => {
-                        let id = getParentId(submenuItem);
-                        return(id === menuItem.uuid && <li className="nav-item" key={menuItem.drupal_internal__id}>
-                          <a className="nav-link px-md-3" href={submenuItem.link.url}>
-                            {submenuItem.title}
-                          </a>
-                          {subMenu && <ul className="tertiaryMenu">
-                            {subMenu.map(item => {
-                              let id = getParentId(item);
-                              return( id === submenuItem.uuid && <li className="nav-item" key={menuItem.drupal_internal__id}>
-                                <a className="nav-link px-md-3" href={item.link.url}>
-                                  {item.title}
-                                </a>
-                              </li>
-                              )})}
-                          </ul>}
-                        </li>)
-                      })}
-                      </ul>}
-                    </li>
-                  )
-                })}
-              </ul>
-            </nav>
-          }
+      <nav className="navbar fixed-top navbar-expand-lg ezcontent-header">
+        <div className="container">
+          <a className="navbar-brand logo" href="/">
+            <img src="/logo.svg" />
+          </a>
+          <button
+            className="navbar-toggler navbar-toggler-right navbar-light"
+            type="button"
+            data-toggle="collapse"
+            data-target="#CollapsingNavbar"
+            aria-controls="navbar-collapse"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div id="CollapsingNavbar" className="navbar-collapse collapse">
+            {headerData &&
+              <nav className="form-inline navbar-form">
+                <ul className="navbar-nav nav">
+                  {headerData.map(menuItem => {
+                    return (
+                      !menuItem.parent && <li className="nav-item" key={menuItem.drupal_internal__id}>
+                        <a className="nav-link px-md-3" href={menuItem.link.url}>
+                          {menuItem.title}
+                        </a>
+                        {subMenu && <ul className="submenu">
+                          {subMenu.map(submenuItem => {
+                            let id = getParentId(submenuItem);
+                            return (id === menuItem.uuid && <li className="nav-item" key={menuItem.drupal_internal__id}>
+                              <a className="nav-link px-md-3" href={submenuItem.link.url}>
+                                {submenuItem.title}
+                              </a>
+                              {subMenu && <ul className="tertiaryMenu">
+                                {subMenu.map(item => {
+                                  let id = getParentId(item);
+                                  return (id === submenuItem.uuid && <li className="nav-item" key={menuItem.drupal_internal__id}>
+                                    <a className="nav-link px-md-3" href={item.link.url}>
+                                      {item.title}
+                                    </a>
+                                  </li>
+                                  )
+                                })}
+                              </ul>}
+                            </li>)
+                          })}
+                        </ul>}
+                      </li>
+                    )
+                  })}
+                </ul>
+              </nav>
+            }
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
     </>
   );
-  
+
 };
